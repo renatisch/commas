5a07c0146ed373a956f20f02e66b9321f7a0fcf0
Renat Ishmukhamedov <renat.isch@gmail.com>
2024-07-26 17:07:06 +0200

    added scheduler

--- app.py
+++ app.py
@@ -1,13 +1,18 @@
 from bots import get_bots_info
 from deals import get_deals_info
-import dotenv
+import dotenv, datetime, time, schedule
 
 dotenv.load_dotenv()
 
 
 def export_data():
+    print(f"Started exporting data at {datetime.datetime.now()}")
     get_bots_info()
     get_deals_info()
+    print(f"Finished exporting data at {datetime.datetime.now()}")
 
 
-export_data()
+schedule.every(10).minutes.do(export_data)
+while True:
+    schedule.run_pending()
+    time.sleep(1)
